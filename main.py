dadc75ff0299e24f29936d91467224b6a3dfc28b
Saul Sandoval <ssandovalsalazar@gmail.com>
2022-06-28 19:02:11 -0500

    There is 2 particles, but...
    The particles do not move when they crash against a wall

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
 from matplotlib import pyplot as plt
 import random
+import particle_object
 
 if __name__ == "__main__":
-    #wallSize is the size of the box
+    #wallSize is the size of the box where particles bounce
     wallSize=10
 
+    
     #x and y are the positions in x-axis and y-axis
-    #These are the initial positions
-    posX=5
-    posY=3
-    print(0,posX,posY)
+    #These are the initial positions of a particle
+    particula1=particle_object.Particle()
+    particula1.posicion(5,3)
+    particula1.velocity(0.5,0.05)
+
+    particula2=particle_object.Particle()
+    particula2.posicion(-2,7)
+    particula2.velocity(0.4,-0.3)
+    """ Second particle. Implementation not yet.
+    particula2=particle_object.Particle()
+    particula2.posicion(-4,-2)
+    particula2.velocity(random.random(),random.random())
+    pos_x=particula1.pos_xaxis
+    pos_y=particula1.pos_yaxis
     #velX and velY are the initial step in each axis. These is 
     #random. 
-    velX=random.random()
-    velY=random.random()
+    vel_x=particula1.vel_xaxis
+    vel_y=particula1.vel_yaxis
+    """
     #velX=0.838921681345721 #random.random()
+    #I tried to make for two particles, It would be more complicated
     #velY=0.186587084087627 #random.random()
     #The particle gives 100 steps
     for i in range(0,100):
@@ -26,31 +40,119 @@ if __name__ == "__main__":
         axes.set_xlim([-10,10])
         axes.set_ylim([-10,10])
         plt.axis('off')
-        posX=posX+velX
-        posY=posY+velY
+        """
+        pos_x=particula1.pos_xaxis
+        pos_y=particula1.pos_yaxis
+        #velX and velY are the initial step in each axis. These is 
+        #random. 
+        vel_x=particula1.vel_xaxis
+        vel_y=particula1.vel_yaxis
+        
+        hay que cambiar estas pos_X a pos_xaxis
+        y lo mismo para el caso del y
+        Y hacelo para ambas particulas
+        De esta forma todo se queda estático, siempre se 
+        redeclara en las posiciones inciales, ya que el atributo
+        de cada instancia no cambia. 
+        
+        tal vez es necesario hacerlo todo dentro de un loop
+        para más partículas
 
-        if posX>wallSize or posX<-wallSize:
-            slope=(posY-(posY-velY))/(posX-(posX-velX))            
-            if posX<-wallSize:
-                posY=-slope*wallSize-slope*posX+posY
-                posX=-wallSize
-            if posX>wallSize:
-                posY=slope*wallSize-slope*posX+posY
-                posX=wallSize
-            velX=-velX
+        """
+        vel_x=particula1.vel_xaxis
+        vel_y=particula1.vel_yaxis        
+        pos_x=particula1.pos_xaxis+vel_x
+        pos_y=particula1.pos_yaxis+vel_y
 
-        if posY>wallSize or posY<-wallSize:
-            slope=(posY-(posY-velY))/(posX-(posX-velX))
+        if pos_x>wallSize or pos_x<-wallSize:
+            slope=(pos_y-(pos_y-vel_y))/(pos_x-(pos_x-vel_x))            
+            if pos_x<-wallSize:
+                pos_y=-slope*wallSize-slope*pos_x+pos_y
+                pos_x=-wallSize
+            if pos_x>wallSize:
+                pos_y=slope*wallSize-slope*pos_x+pos_y
+                pos_x=wallSize
+            particula1.vel_xaxis=-vel_x
             
-            if posY<-wallSize:
-                posX=-wallSize/slope-posY/slope+posX
-                posY=-wallSize
-            if posY>wallSize:
-                posX=wallSize/slope-posY/slope+posX
-                posY=wallSize
-            velY=-velY
+        #This conditional makes ball bounce in the superior and inferior faces
+        if pos_y>wallSize or pos_y<-wallSize:
+            slope=(pos_y-(pos_y-vel_y))/(pos_x-(pos_x-vel_x))
+            if pos_y<-wallSize:
+                pos_x=-wallSize/slope-pos_y/slope+pos_x
+                pos_y=-wallSize
+            if pos_y>wallSize:
+                pos_x=wallSize/slope-pos_y/slope+pos_x
+                pos_y=wallSize
+            particula1.vel_yaxis=-vel_y
+            
         #print(i+1,posX,posY)
-        plt.plot(posX,posY,"bo-")
+        plt.plot(pos_x,pos_y,"bo-")
+        particula1.pos_xaxis=pos_x
+        particula1.pos_yaxis=pos_y
+        
+        vel_x=particula2.vel_xaxis
+        vel_y=particula2.vel_yaxis        
+        pos_x=particula2.pos_xaxis+vel_x
+        pos_y=particula2.pos_yaxis+vel_y
+
+        if pos_x>wallSize or pos_x<-wallSize:
+            slope=(pos_y-(pos_y-vel_y))/(pos_x-(pos_x-vel_x))            
+            if pos_x<-wallSize:
+                pos_y=-slope*wallSize-slope*pos_x+pos_y
+                pos_x=-wallSize
+            if pos_x>wallSize:
+                pos_y=slope*wallSize-slope*pos_x+pos_y
+                pos_x=wallSize
+            particula1.vel_xaxis=-vel_x
+            
+        #This conditional makes ball bounce in the superior and inferior faces
+        if pos_y>wallSize or pos_y<-wallSize:
+            slope=(pos_y-(pos_y-vel_y))/(pos_x-(pos_x-vel_x))
+            if pos_y<-wallSize:
+                pos_x=-wallSize/slope-pos_y/slope+pos_x
+                pos_y=-wallSize
+            if pos_y>wallSize:
+                pos_x=wallSize/slope-pos_y/slope+pos_x
+                pos_y=wallSize
+            particula1.vel_yaxis=-vel_y
+            
+        #print(i+1,posX,posY)
+        particula2.pos_xaxis=pos_x
+        particula2.pos_yaxis=pos_y
+        plt.plot(pos_x,pos_y,"ro-")
+        """
+        pos_x=particula2.pos_xaxis
+        pos_y=particula2.pos_yaxis
+        #velX and velY are the initial step in each axis. These is 
+        #random. 
+        vel_x=particula2.vel_xaxis
+        vel_y=particula2.vel_yaxis
+        pos_x=pos_x+vel_x
+        pos_y=pos_y+vel_y
+
+        if pos_x>wallSize or pos_y<-wallSize:
+            slope=(pos_y-(pos_y-vel_y))/(pos_x-(pos_x-vel_x))            
+            if pos_x<-wallSize:
+                pos_y=-slope*wallSize-slope*pos_x+pos_y
+                pos_x=-wallSize
+            if pos_x>wallSize:
+                pos_y=slope*wallSize-slope*pos_x+pos_y
+                pos_x=wallSize
+            vel_x=-vel_x
+
+        if pos_y>wallSize or pos_y<-wallSize:
+            slope=(pos_y-(pos_y-vel_y))/(pos_x-(pos_x-vel_x))
+            
+            if pos_y<-wallSize:
+                pos_x=-wallSize/slope-pos_y/slope+pos_x
+                pos_y=-wallSize
+            if pos_y>wallSize:
+                pos_x=wallSize/slope-pos_y/slope+pos_x
+                pos_y=wallSize
+            vel_y=-vel_y
+        #print(i+1,posX,posY)
+        plt.plot(pos_x,pos_y,"ro-")
+        """
         if i<=9:
             plt.savefig(f'line plot0{i}.jpg', bbox_inches='tight', dpi=150)
         if i>=10:
--- make_gif.py
+++ make_gif.py
@@ -2,10 +2,10 @@ import glob
 
 from PIL import Image
 
-def makegif(folder):
+def make_gif(folder):
     frames = [Image.open(image) for image in glob.glob(f"{folder}/*.jpg")]
     frame_one =frames[0]
     frame_one.save("my_awesome.gif", format="GIF", append_images=frames,
             save_all=True, duration=100, loop=0)
 
-makegif("d:/SASS/colisions")
+make_gif("d:/SASS/colisions")
